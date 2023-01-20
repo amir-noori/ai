d4b64634f81b88fbbf16781dd38174b656b7894a
Amir Noori <a.noori@behsacorp.com>
2023-01-20 09:57:09 +0330

    initial commit

---
+++ src/config.py
@@ -0,0 +1,5 @@
+
+class Config:
+
+    BASE_DIR = "D:\\src\\py\\ai"
+    DATA_DIR = "D:\\src\\py\\ai\\data"
---
+++ src/ml/online_shoppers_intention/run.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly as py
+import plotly.graph_objs as go
+from sklearn.cluster import KMeans
+
+# read the dataset
+data = pd.read_csv('../../../data/online_shoppers_intention.csv')
+
+wcss = []
+for i in range(1, 11):
+    km = KMeans(n_clusters = i, init = 'k-means++', max_iter = 300, n_init = 10, random_state = 0, algorithm = 'full', tol = 0.001)
+    km.fit(x)
+    labels = km.labels_
+    wcss.append(km.inertia_)
+    
+plt.rcParams['figure.figsize'] = (13, 7)
+plt.plot(range(1, 11), wcss)
+plt.grid()
+plt.tight_layout()
+plt.title('The Elbow Method', fontsize = 20)
+plt.xlabel('No. of Clusters')
+plt.ylabel('wcss')
+plt.show()
